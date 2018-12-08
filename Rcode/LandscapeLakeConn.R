ac22cbf60287e3cc3cebf69246cbe9e2fa9032e1
immccull95 <imccullough@bren.ucsb.edu>
2018-12-08 12:50:58 -0500

    trying to get ##$5%# function right

--- Rcode/LandscapeLakeConn.R
+++ Rcode/LandscapeLakeConn.R
@@ -1,6 +1,6 @@
 ####################### Landscape connectivity of lakes ########################################
 # Date: 11-8-18
-# updated: 12-6-18
+# updated: 12-8-18
 # Author: Ian McCullough, immccull@gmail.com
 ################################################################################################
 
@@ -36,7 +36,7 @@ zonal_cost_dist <- read.csv("C:/Ian_GIS/FreshwaterConservation/Exports/MI_CostDi
 # PADUS raster (protected areas database)
 PADUS <- raster("C:/Ian_GIS/FreshwaterConservation/Exports/MI_PADUS.tif")
 
-# Pre-calculated patch metric data
+# Pre-calculated patch metric data (from SDMTools::PatchStat)
 LakePatchMetrics <- read.csv("Data/MichiganLakePatchStats.csv")
 
 ##### D-fine functions #####
@@ -57,19 +57,30 @@ cellsize <- 30 #m cell width
 # test function
 # use all 4ha lakes so can account for connectivity into neighboring states (may need to remove Ontario border lakes eventually, though)
 # but would need patchstats for those states...
-whee <- lake_patch_metrics(lagoslakeid=95687, lake_shp=lakes_4ha_poly, cellsize=30, dispersal_buff=dispersal_buff, patchmetric_df=LakePatchMetrics)
+whee <- lake_patch_metrics(lagoslakeid=99862, lake_shp=lakes_4ha_poly, cellsize=30, dispersal_buff=dispersal_buff, patchmetric_df=LakePatchMetrics)
 
 # create empty data frame, with each iteration filling in a row
-LakeBufferPatchStatz <- data.frame(matrix(NA, nrow = length(mich_lagoslakeids), ncol = 12))
-colnames(LakeBufferPatchStatz) <- c('lagoslakeid','FullLakeArea_ha','FullLakePerimeter_km','FullLakeEdgeArea_ha','FullLakeCoreArea_ha',
-                                    'AvgFullLakeShapeIndex','AvgFullLakeCoreAreaIndex','nLakePatches','BuffArea_ha','EdgeLengthInBuff_km',
-                                    'EdgeAreaInBuff_ha','EdgeAreaInBuff_pct')
+LakeBufferPatchStatz <- data.frame(matrix(NA, nrow = length(mich_lagoslakeids), ncol = 13))
+colnames(LakeBufferPatchStatz) <- c('lagoslakeid','FullLakeArea_ha','FullLakePerimeter_km','FullLakeEdgeArea_ha',
+                                    'AvgFullLakeShapeIndex','FullLakeCoreArea_ha','AvgFullLakeCoreAreaIndex','nLakePatches','BuffArea_ha','EdgeLengthInBuff_km',
+                                    'EdgeAreaInBuff_ha','EdgeAreaInBuff_pct','BufferWidth_m')
 for (i in 1:length(mich_lagoslakeids)){
   tump <- lake_patch_metrics(lagoslakeid=mich_lagoslakeids[i], lake_shp=lakes_4ha_poly, dispersal_buff=dispersal_buff, cellsize=cellsize, patchmetric_df=LakePatchMetrics)
   LakeBufferPatchStatz[i,] <- tump
   tump <- NULL
 }
 
+# tripped: 
+# lagoslakeid: 23961
+# 62520: erase lake from buffer generates empty output, causing error, even though lake and buffer appear to overlap in plot()
+# 1362: ran manually, worked...not sure of issue. Seems to be trying to process non-existent intersecting lakes despite conditional
+# 47003: did not check why...
+
+for (i in 3938:length(mich_lagoslakeids)){
+  tump <- lake_patch_metrics(lagoslakeid=mich_lagoslakeids[i], lake_shp=lakes_4ha_poly, dispersal_buff=dispersal_buff, cellsize=cellsize, patchmetric_df=LakePatchMetrics)
+  LakeBufferPatchStatz[i,] <- tump
+  tump <- NULL
+}
 
 ############# Patch stats on Michigan lakes ###########
 # Calculated as if lakes are patches in the same way as terrestrial patches
--- Rcode/functions/lake_patch_metrics.R
+++ Rcode/functions/lake_patch_metrics.R
@@ -4,14 +4,15 @@
 # FullLakeArea_ha: total lake area of lakes intersecting with buffer (including area outside buffer)
 # FullLakePerimeter_km: total lake perimeter of lakes intersecting with buffer (including lengths outside buffer)
 # FullLakeEdgeArea_ha: total edge area of lakes intersecting with buffer (including lake area outside buffer)
-# FullLakeCoreArea_ha: total core area of lakes intersecting with buffer (including lake area outside buffer)
 # AvgFullLakeShapeIndex: average shape index of lakes intersecting with buffer (including lake area outside buffer)
+# FullLakeCoreArea_ha: total core area of lakes intersecting with buffer (including lake area outside buffer)
 # AvgFullLakeCoreAreaIndex: average core area index of lakes intersecting with buffer (including lake area outside buffer)
 # nLakePatches: number of lakes (patches) within buffer
 # BuffArea_ha: area of buffer zone
 # EdgeLengthInBuff_km: total edge length (inside buffer only) of lakes intersecting with buffer
 # EdgeAreaInBuff_ha: total area of edge habitat (inside buffer only) of lakes intersecting with buffer (1-cell deep in lake polygons, calculated using cellsize argument)
 # EdgeAreaInBuff_pct: proportion of buffer area covered by edge habitat
+# BufferWidth_m: taken from dispersal_buff argument (as record in output)
 library(raster)
 library(rgeos)
 library(spatialEco)
@@ -23,43 +24,45 @@ lake_patch_metrics <- function(lagoslakeid, lake_shp, dispersal_buff, cellsize, 
   #cellsize: pixel width of analysis
   #patchmetric_df: output of SDMTools::PatchStat for lake_shp lakes (or subset)
   
-  # extract focal lake, buffer it by dispersal width, erase focal lake from buffer zone
+  # extract focal lake, buffer it by dispersal width, get buffer area as buffer area minus focal lake area
   focal_lake <- lagoslakeid
   focal_lake_shp <- subset(lake_shp, lagoslakei %in% lagoslakeid)
   focal_lake_buff <- gBuffer(focal_lake_shp, byid=T, width=dispersal_buff)
-  focal_lake_buff_erased <- erase(focal_lake_buff, focal_lake_shp)
-  focal_lake_buff_erased_area <- gArea(focal_lake_buff_erased)
+  focal_lake_buff_area_ha <- (gArea(focal_lake_buff) - gArea(focal_lake_shp))/10000
   
-  # identify lakes that intersect with buffer (only lake sections within buffer)
-  # build in conditional statements:
-  #1) if intersecting lakes do not exist, return empty output
-  #2) if intersecting lakes do exist, calculate area/length of edge habitat within buffer and pull in patch stats from patchmetric_df
-  intersecting_lakes <- raster::intersect(lake_shp, focal_lake_buff_erased)
+  # identify lakes that intersect with buffer, excluding focal lake (length=1 if buffer only contains focal lake)
+  intersecting_lakes <- raster::intersect(lake_shp, focal_lake_buff)
   
-  if (is.null(intersecting_lakes) == T) {
+  if (length(intersecting_lakes) <= 1) {
     output_df <- data.frame(lagoslakeid=lagoslakeid, FullLakeArea_ha=0, FullLakePerimeter_km=0,
-                            FullLakeEdgeArea_ha=0, FullLakeCoreArea_ha=0, AvgFullLakeShapeIndex=NA, AvgFullLakeCoreAreaIndex=NA,
-                            nLakePatches=0, BuffArea_ha=focal_lake_buff_erased_area/10000, EdgeLengthInBuff_km=0, EdgeAreaInBuff_ha=0, EdgeAreaInBuff_pct=0,
-                            stringsAsFactors=F) #stringsAsFactors prevents factor level from being returned over character value
+                            FullLakeEdgeArea_ha=0, AvgFullLakeShapeIndex=NA, FullLakeCoreArea_ha=0, AvgFullLakeCoreAreaIndex=NA,
+                            nLakePatches=0, BuffArea_ha=focal_lake_buff_area_ha, EdgeLengthInBuff_km=0, EdgeAreaInBuff_ha=0, EdgeAreaInBuff_pct=0,
+                            BufferWidth_m=dispersal_buff, stringsAsFactors=F) #stringsAsFactors prevents factor level from being returned over character value
     rownames(output_df) <- lagoslakeid
+    
   } else {
-    EdgeLength_m <- (spatialEco::polyPerimeter(intersecting_lakes))
-    EdgeArea_sqm <- EdgeLength_m * (cellsize^2)
+    # Subtract perimeter of focal lake from total perimeter of intersecting lakes (round to make tiny numbers 0)
+    EdgeLength_km <- round(((sum(spatialEco::polyPerimeter(intersecting_lakes), na.rm=T) - spatialEco::polyPerimeter(focal_lake_shp))/1000),4)
+    # get edge habitat area by getting shoreline length in meters, divided by cell size to get number of cells, which then gets multiplied by cell area
+    EdgeArea_ha <- (((EdgeLength_km*1000)/cellsize) * (cellsize^2))/10000
+
     # Bring in full lake patch data for intersecting lakes from other df (output of SDMTools::PatchStat)
     intersecting_lakes_IDs <- unique(intersecting_lakes$lagoslakei.1)
+    intersecting_lakes_IDs <- intersecting_lakes_IDs[ intersecting_lakes_IDs != lagoslakeid ]#remove focal lake
     intersecting_lakes_metrics <- subset(patchmetric_df, patchID %in% intersecting_lakes_IDs)
     intersecting_lakes_metrics <- intersecting_lakes_metrics[,c('patchID','area','perimeter','edge.area','shape.index','core.area','core.area.index')]
-    colnames(intersecting_lakes_metrics) <- c('lagoslakeid','FullLakeArea_sqm','FullLakePerimeter_m','FullLakeEdgeArea_sqm','FullLakeCoreArea_ha','AvgFullLakeShapeIndex','AvgFullLakeCoreAreaIndex')
+    colnames(intersecting_lakes_metrics) <- c('lagoslakeid','FullLakeArea_sqm','FullLakePerimeter_m','FullLakeEdgeArea_sqm','AvgFullLakeShapeIndex','FullLakeCoreArea_ha','AvgFullLakeCoreAreaIndex')
     bcde <- colSums(intersecting_lakes_metrics[,2:5], na.rm=T)
     fg <- colMeans(intersecting_lakes_metrics[,6:7], na.rm=T)
     nLakePatches <- nrow(intersecting_lakes_metrics)
     
     output_df <- data.frame(lagoslakeid=lagoslakeid, FullLakeArea_ha=bcde[1]/10000, FullLakePerimeter_km=bcde[2]/1000,
-                            FullLakeEdgeArea_ha=bcde[3]/10000, FullLakeCoreArea_ha=bcde[4], AvgFullLakeShapeIndex=fg[1], AvgFullLakeCoreAreaIndex=fg[2],
-                            nLakePatches=nLakePatches, BuffArea_ha=focal_lake_buff_erased_area/10000, stringsAsFactors=F) #stringsAsFactors prevents factor level from being returned over character value)
-    output_df$EdgeLengthInBuff_km <- sum(EdgeLength_m, na.rm=T)/1000
-    output_df$EdgeAreaInBuff_ha <- sum(EdgeArea_sqm, na.rm=T)/10000
+                            FullLakeEdgeArea_ha=bcde[3]/10000, AvgFullLakeShapeIndex=bcde[4], FullLakeCoreArea_ha=fg[1], AvgFullLakeCoreAreaIndex=fg[2],
+                            nLakePatches=nLakePatches, BuffArea_ha=focal_lake_buff_area_ha, stringsAsFactors=F) #stringsAsFactors prevents factor level from being returned over character value)
+    output_df$EdgeLengthInBuff_km <- sum(EdgeLength_km, na.rm=T)
+    output_df$EdgeAreaInBuff_ha <- sum(EdgeArea_ha, na.rm=T)
     output_df$EdgeAreaInBuff_pct <- output_df$EdgeAreaInBuff_ha/output_df$BuffArea_ha
+    output_df$BufferWidth_m <- dispersal_buff
     rownames(output_df) <- lagoslakeid
     
   }
@@ -73,9 +76,9 @@ lake_patch_metrics <- function(lagoslakeid, lake_shp, dispersal_buff, cellsize, 
   nLakePatches <- NULL
   focal_lake <- NULL
   focal_lake_shp <- NULL
-  focal_lake_buff_erased <- NULL
-  focal_lake_buff_erased_area <- NULL
-  EdgeLength_m <- NULL
-  EdgeArea_sqm <- NULL
+  focal_lake_buff <- NULL
+  focal_lake_buff_area_ha <- NULL
+  EdgeLength_km <- NULL
+  EdgeArea_ha <- NULL
   output_df <- NULL
 }
