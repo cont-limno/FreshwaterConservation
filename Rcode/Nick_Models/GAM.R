9692d08bd6e8b17a1f96ff385d9c9f9b051e3f33
nskaff <nicholas.skaff@gmail.com>
2019-01-08 21:56:53 -0800

    GAM plots

--- Rcode/Nick_Models/GAM.R
+++ Rcode/Nick_Models/GAM.R
@@ -44,14 +44,15 @@ for (j in 1:6){
     r2<-sum$r.sq
     edf<-sum$edf
     
-    png(filename=paste("~/Documents/Grad School/Projects/FreshwaterConservation/Data/Nick/Results/GAM/",paste(response,covariate, sep="_") ,".png", sep=""))
+    png(filename=paste("Data/Nick/Results/GAM/",paste(response,covariate, sep="_") ,".png", sep=""))
     plot(gam_model, xlab=paste(covariate,"Scaled", sep=" "), ylab="Log Odds Not Protected", main=response)
     dev.off()
   }
     
     gam_results<-rbind(gam_results,data.frame(response=response,covariate=covariate, p=p, dev_expl=dev_expl, r2=r2, edf=edf))
-    print(j)
+    print(i)
   }
+  print(j)
 }
 
 
@@ -59,15 +60,25 @@ for (j in 1:6){
 responses<-as.character(unique(gam_results$response))
 for (j in 1:length(responses)){
   
-  plot_dat<-gam_results[gam_results$response==responses[j],]
+  plot_dat_Ws<-gam_results[gam_results$response==responses[j] ,]
+  plot_dat_Cat<-gam_results[gam_results$response==responses[j],]
   
   covs_order = factor(plot_dat$covariate, levels=unique(plot_dat$covariate[order(plot_dat$r2)]), ordered=TRUE)
   
   plot_dat$covs_order<-covs_order
   
-  plot<-ggplot(data=plot_dat) + geom_point(aes(x=covs_order, y=r2))  + geom_hline(yintercept=0) + theme_classic() + scale_y_continuous("Model R Squared")+ scale_x_discrete("")+ theme(axis.text.x = element_text(angle = 70, hjust = 1)) + ggtitle(responses[j])
+  plot_Ws<-ggplot(data=plot_dat[grep("Ws",plot_dat$covariate),]) + geom_point(aes(x=covs_order, y=r2), size=2)   + theme_classic() + scale_y_continuous("Adjusted R Squared", limits=c(0,.23))+ scale_x_discrete("")+ theme(axis.text.x = element_text(angle = 70, hjust = 1)) + ggtitle(responses[j])
   
-  ggsave(filename=paste(responses[j],".png", sep=""),plot=plot, path="~/Documents/Grad School/Projects/FreshwaterConservation/Data/Nick/Results/GAM/fit_plots",device="png",width=6.5, height=4.5, dpi=300, units="in" )
+  ggsave(filename=paste(responses[j],"_Ws",".png", sep=""),plot=plot_Ws, path="Data/Nick/Results/GAM/fit_plots",device="png",width=6.5, height=4.5, dpi=300, units="in" )
+  
+  plot_Cat<-ggplot(data=plot_dat[grep("Cat",plot_dat$covariate),]) + geom_point(aes(x=covs_order, y=r2), size=2)   + theme_classic() + scale_y_continuous("Adjusted R Squared", limits=c(0,.23))+ scale_x_discrete("")+ theme(axis.text.x = element_text(angle = 70, hjust = 1)) + ggtitle(responses[j])
+  
+  ggsave(filename=paste(responses[j],"_Cat",".png", sep=""),plot=plot_Cat, path="Data/Nick/Results/GAM/fit_plots",device="png",width=6.5, height=4.5, dpi=300, units="in" )
+  
+  plot<-ggplot(data=plot_dat) + geom_point(aes(x=covs_order, y=r2), size=2)   + theme_classic() + scale_y_continuous("Adjusted R Squared", limits=c(0,.23))+ scale_x_discrete("")+ theme(axis.text.x = element_text(angle = 70, hjust = 1)) + ggtitle(responses[j])
+  
+  ggsave(filename=paste(responses[j],"_Ws_Cat",".png", sep=""),plot=plot, path="Data/Nick/Results/GAM/fit_plots",device="png",width=6.5, height=4.5, dpi=300, units="in" )
+  
   
 }
 
