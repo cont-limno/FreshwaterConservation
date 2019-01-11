c0622d06edf0818ab7a79490e683a8496c39cc09
jsta <stachel2@msu.edu>
2019-01-11 17:06:01 -0500

    lakes intersecting the great lakes are bad

--- Rcode/get_inland-lakes.R
+++ Rcode/get_inland-lakes.R
@@ -6,6 +6,7 @@ library(nhdR)
 library(sf)
 library(dplyr)
 library(units)
+library(mapview) # optional
 
 # ---- load_gis_data ----
 
@@ -16,6 +17,17 @@ library(units)
 mich_shp       <- query_gis("STATE", "State_Name", "Michigan")
 mich_lakes_4ha <- query_gis_(query = "SELECT * FROM LAGOS_NE_All_Lakes_4ha WHERE STATE LIKE 'MI'")
 
+# remove lakes that intersect the Great Lakes
+great_lakes <- great_lakes(spatial = TRUE)
+bad_lakes <- mich_lakes_4ha[
+  unlist(lapply(
+    st_intersects(mich_lakes_4ha, st_buffer(gl, 20)), 
+    function(x) length(x) > 0)),]
+
+# remove lakes in LAGOS that are not in NHDPlus
+# llid <- 2227
+# TODO
+
 # xwalk          <- load_xwalk()
 # mich_lakes_4ha <- left_join(mich_lakes_4ha, 
 #                             dplyr::select(xwalk, "lagoslakeid", 
@@ -23,31 +35,22 @@ mich_lakes_4ha <- query_gis_(query = "SELECT * FROM LAGOS_NE_All_Lakes_4ha WHERE
 
 # ---- find_bad_lakes -----
 
-## Houghton Lake is the largest MI inland lake
-## use it to generate a polyline selection buffer
-# 
-# coords <- lake_info(name = "Houghton Lake", state = "Michigan")
-# 
-# units::set_units(
-#   units::set_units(
-#     lake_info(name = "Houghton Lake", state = "Michigan")$lake_area_ha, 
-#     "ha"), "dd")
-# 
-# greatlakes_lines <- nhdR::nhd_plus_query(lat = coords$nhd_lat,
-#                                          lon = coords$nhd_long,
-#                                          dsn = "NHDFlowLine",
-#                                          buffer_dist = 0.1)$sp$NHDFlowLine
+# Check out the following possible bad lakes:
+# llid <- 2639
+# llid <- 2197
 
-is_bad_lake <- function(llid){
-  llid        <- 2639 # should be a bad lake
-  coords      <- lake_info(lagoslakeid = llid)[,c("nhd_long", "nhd_lat")]
+is_bad_lake <- function(llid, buffer_size){
+  coords      <- lake_info(lagoslakeid = llid)
   focal_poly  <- dplyr::filter(mich_lakes_4ha, lagoslakeid == llid)
+  buffer_size <- units::set_units(
+    sqrt(coords$lake_area_ha * 10000) * 1.2,
+    "m")
   
-  # get intersecting flowlines, are they all FTYPE coastal?
+  # get intersecting flowlines, bad lake if they are all FTYPE coastal
   focal_lines <- nhdR::nhd_plus_query(lat = coords$nhd_lat,
-                                           lon = coords$nhd_long,
-                                           dsn = "NHDFlowLine",
-                                           buffer_dist = 0.1)$sp$NHDFlowLine
+                                    lon = coords$nhd_long,
+                                    dsn = "NHDFlowLine",
+                                    buffer_dist = buffer_size)$sp$NHDFlowLine
   focal_lines <- st_transform(focal_lines, st_crs(focal_poly))
   focal_lines <- focal_lines[
     unlist(lapply(
@@ -56,9 +59,10 @@ is_bad_lake <- function(llid){
   all_coastal <- all(focal_lines$FTYPE == "Coastline")
   
   if(!all_coastal){
-    # walk upstream
-    # bad lake if we get FTYPE coastal reaches
-    nhdR::terminal_reaches(lon = coords$nhd_long, lat = coords$nhd_lat)
+    # walk upstream, bad lake if we get any FTYPE coastal reaches
+    test <- nhdR::extract_network(lon = coords$nhd_long, lat = coords$nhd_lat)
+    
+    
     
   }
   
@@ -68,10 +72,7 @@ is_bad_lake <- function(llid){
   
 }
 
-#Check out the following possible bad lakes:
-# lagoslakeid != 2227,
-# lagoslakeid != 2197,
-#lagoslakeid != 4930
+
 
 # get lakes that only contain flowlines with a "Coastal" FTYPE
 # get flowlines that intersect the boundary of the Michigan landmass 
