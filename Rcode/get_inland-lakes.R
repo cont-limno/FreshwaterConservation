4ab49b36f76cba9b1617e5730e5623c6e76cdab0
jsta <stachel2@msu.edu>
2019-01-12 12:11:19 -0500

    rm lakes not in nhdplus, and isolated lakes

--- Rcode/get_inland-lakes.R
+++ Rcode/get_inland-lakes.R
@@ -18,28 +18,41 @@ mich_shp       <- query_gis("STATE", "State_Name", "Michigan")
 mich_lakes_4ha <- query_gis_(query = "SELECT * FROM LAGOS_NE_All_Lakes_4ha WHERE STATE LIKE 'MI'")
 
 # remove lakes that intersect the Great Lakes
-great_lakes <- great_lakes(spatial = TRUE)
+# llid <- 2639
+# llid <- 2197
+gl <- great_lakes(spatial = TRUE)
 bad_lakes <- mich_lakes_4ha[
   unlist(lapply(
     st_intersects(mich_lakes_4ha, st_buffer(gl, 20)), 
     function(x) length(x) > 0)),]
+bad_lakes$reason <- "intersect GL"
 
 # remove lakes in LAGOS that are not in NHDPlus
 # llid <- 2227
-# TODO
+xwalk <- load_xwalk()
+not_in_nhdp <- left_join(mich_lakes_4ha, 
+                         dplyr::select(xwalk, -GNIS_Name, -GNIS_ID), 
+                         by = "lagoslakeid") %>% 
+  dplyr::filter(is.na(NHDPlusv2_COMID)) %>%
+  dplyr::select(names(bad_lakes)[-which(names(bad_lakes) == "reason")]) %>%
+  dplyr::filter(!(lagoslakeid %in% bad_lakes$lagoslakeid))
+not_in_nhdp$reason <- "not in NHDp"
+bad_lakes <- rbind(bad_lakes, not_in_nhdp)
 
-# xwalk          <- load_xwalk()
-# mich_lakes_4ha <- left_join(mich_lakes_4ha, 
-#                             dplyr::select(xwalk, "lagoslakeid", 
-#                                           "NHDHR_PermanentIdentifier"))
+# remove isolated lakes
+# llid <- 2227
+isolated_lakes <- dplyr::filter(mich_lakes_4ha, 
+                                LakeConnectivity == "Isolated")
+isolated_lakes$reason <- "isolated"
+bad_lakes <- rbind(bad_lakes, isolated_lakes)
+bad_lakes_llids <- bad_lakes$lagoslakeid
 
+test <- dplyr::filter(mich_lakes_4ha, 
+                      !(lagoslakeid %in% bad_lakes$lagoslakeid))
+
+llid <- test$lagoslakeid[5]
 # ---- find_bad_lakes -----
-
-# Check out the following possible bad lakes:
-# llid <- 2639
-# llid <- 2197
-
-is_bad_lake <- function(llid, buffer_size){
+is_bad_lake <- function(llid){
   coords      <- lake_info(lagoslakeid = llid)
   focal_poly  <- dplyr::filter(mich_lakes_4ha, lagoslakeid == llid)
   buffer_size <- units::set_units(
@@ -51,28 +64,46 @@ is_bad_lake <- function(llid, buffer_size){
                                     lon = coords$nhd_long,
                                     dsn = "NHDFlowLine",
                                     buffer_dist = buffer_size)$sp$NHDFlowLine
-  focal_lines <- st_transform(focal_lines, st_crs(focal_poly))
-  focal_lines <- focal_lines[
-    unlist(lapply(
-      st_intersects(focal_lines, focal_poly),
-      function(x) length(x) > 0)),]
-  all_coastal <- all(focal_lines$FTYPE == "Coastline")
+  if(nrow(focal_lines) > 0){
+    focal_lines <- st_transform(focal_lines, st_crs(focal_poly))
+    focal_lines <- focal_lines[
+      unlist(lapply(
+        st_intersects(focal_lines, focal_poly),
+        function(x) length(x) > 0)),]
+    all_coastal <- all(focal_lines$FTYPE == "Coastline")
+  }else{
+    print("Lake is not connected to NHDPlus streams")
+    all_coastal <- TRUE # lake not connected to nhdplus streams
+  }
   
   if(!all_coastal){
     # walk upstream, bad lake if we get any FTYPE coastal reaches
-    test <- nhdR::extract_network(lon = coords$nhd_long, lat = coords$nhd_lat)
-    
-    
-    
+    up_network <- nhdR::extract_network(
+      lon = coords$nhd_long, 
+      lat = coords$nhd_lat)
+    if(!is.null(nrow(up_network))){
+      downstream_of_gl <- any(up_network$ftype == "Coastal")
+    }else{
+      print("No upnetwork streams found in the NHDPlus")
+      downstream_of_gl <- TRUE
+    }
+  }else{
+    downstream_of_gl <- FALSE
   }
   
-  mapview::mapview(focal_poly) + mapview::mapview(focal_lines)
-  
-  
-  
+  if(!all_coastal & !downstream_of_gl){
+    FALSE # not bad lake
+  }else{
+    TRUE # bad lake
+  }
 }
 
+is_bad_lake(llid)
 
+test2 <- sapply(test$lagoslakeid[1:5], function(x){
+  print(x) 
+  is_bad_lake(x)
+}) 
 
 # get lakes that only contain flowlines with a "Coastal" FTYPE
 # get flowlines that intersect the boundary of the Michigan landmass 
